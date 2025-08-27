2c15ade42fa3e0e866e69e4f46f2b3ec734070e7
Batoolgit <86097826+Batoolgit@users.noreply.github.com>
2025-08-27 07:49:29 +0000

    Task 1.2 insert differentiation functions

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,27 @@ import numpy as np
 
 
 def differentiate(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    d = np.zeros_like(u)
+    for i in range(len(u)):
+        if i == 0:
+            d[i] = (u[1] - u[0]) / dt
+        elif i == len(u)-1:
+         
+            d[i] = (u[i] - u[i - 1]) / dt
+        else:
+        
+            d[i] = (u[i + 1] - u[i - 1]) / (2 * dt)
+    print(d)
+    return d 
 
 def differentiate_vector(u: np.ndarray, dt: float) -> np.ndarray:
-    raise NotImplementedError
+    d= np.zeros_like(u)
+    N_t=len(u)-1
+    d[0] = (u[1] - u[0]) / dt
+    d[1:N_t]=(u[2:N_t+1] -u[0:N_t-1])/(2*dt)
+    d[N_t]=(u[N_t]-u[N_t-1])/dt
+    print(d)
+    return d
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
